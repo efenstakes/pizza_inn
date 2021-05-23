96d41fe3c03bf0f3dbcd921d9768681125b588ba
Efen Stakes <efenstakes101@gmail.com>
2021-05-23 11:49:35 +0300

    add food card component

---
+++ src/components/food_card/food_card.component.tsx
@@ -0,0 +1,44 @@
+import { 
+    IonText, IonTitle, IonImg, IonIcon, IonFabButton, 
+} from '@ionic/react'
+import { cartOutline } from 'ionicons/icons'
+
+import Food from '../../models/food.model'
+import VSpacerComponent from '../v_spacer/v_spacer.component'
+
+import './food_card.component.scss'
+
+interface ComponentProps {
+    food: Food,
+}
+const FoodCardComponent: React.FC<ComponentProps> = ({ food }) => {
+    return (
+        <div className="food_card">
+            <div className="food_card__image__wrapper">
+                <div className="food_card__image_container">
+                    <img
+                        className="food_card__image"
+                        src={food.img}
+                    />
+                </div>
+                <IonFabButton size="small" className="food_card__image_button">
+                    <IonIcon 
+                        icon={cartOutline} 
+                        style={{
+                            fontSize: '1.1rem'
+                        }}
+                    />
+                </IonFabButton>
+            </div>
+            <VSpacerComponent space={2} />
+            <IonTitle>
+                { food.name }
+            </IonTitle>
+            <IonText>
+                { food.price }
+            </IonText>
+        </div>
+    )
+}
+
+export default FoodCardComponent
