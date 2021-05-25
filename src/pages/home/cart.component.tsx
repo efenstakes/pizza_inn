82caddccda2de7727d88d6a5e0bcd6544e85527a
Efen Stakes <efenstakes101@gmail.com>
2021-05-25 13:07:02 +0300

    show cart items with appropriate functions

--- src/pages/home/cart.component.tsx
+++ src/pages/home/cart.component.tsx
@@ -4,8 +4,9 @@ import {
     IonText,
     IonChip, 
 } from "@ionic/react"
-import { pizzaOutline, add, remove } from 'ionicons/icons'
+import { pizzaOutline, add, remove, cart } from 'ionicons/icons'
 import { useState } from "react"
+import CartFoodCardComponent from "../../components/cart_food_card/cart_food_card.component"
 import VSpacerComponent from "../../components/v_spacer/v_spacer.component"
 
 
@@ -15,23 +16,44 @@ import Food from "../../models/food.model"
 interface ComponentProps {
     selectedFood: Food,
     setSelectedFood: Function,
+    cart: Food[],
+    addQuantity: Function,
+    minusQuantity: Function,
+    removeFromCart: Function,
 }
-const CartComponent:React.FC<ComponentProps> = ({ selectedFood, setSelectedFood, }) => {
+const CartComponent:React.FC<ComponentProps> = ({ cart, addQuantity, minusQuantity, removeFromCart, selectedFood, setSelectedFood, }) => {
+    
+    if( cart.length === 0 ) {
+        return (
+            <IonContent>
+                <CartEmptyComponent/>
+            </IonContent>
+        )
+    }
     return (
         <>
-            {/* <IonHeader>
-                <IonToolbar color="primary">
-                    <IonTitle>Menu</IonTitle>
-                </IonToolbar>
-            </IonHeader> */}
             <IonContent>
+                
+                <VSpacerComponent space={6} />
+            
+                <IonTitle>
+                    Your Cart
+                </IonTitle>
+
+                <VSpacerComponent space={2} />
+
                 {
-                    selectedFood &&
-                        <CartContentComponent food={selectedFood} />
-                }
-                {
-                    selectedFood == null &&
-                        <CartEmptyComponent />
+                    cart.map(food=> {
+                        return (
+                            <CartFoodCardComponent
+                                key={food.id}
+                                food={food}
+                                addQuantity={addQuantity}
+                                minusQuantity={minusQuantity}
+                                removeFromCart={removeFromCart}
+                            />
+                        )
+                    })
                 }
             </IonContent>
         </>
@@ -57,156 +79,6 @@ const CartEmptyComponent = ()=> {
         </div>
     )
 }
-
-
-interface CartContentComponentProps {
-    food: Food,
-}
-const CartContentComponent: React.FC<CartContentComponentProps> = ({ food })=> {
-    const sizes = [
-        'small', 'medium', 'large', 'mega'
-    ]
-    const [selectedSize, setSelectedSize] = useState('medium')
-    const [quantity, setQuantity] = useState(1)
-    
-
-    const addQuantity = ()=> setQuantity(quantity+1)
-    const minusQuantity = ()=> quantity > 1 && setQuantity(quantity-1)
-
-    return (
-        <div className="cart_content">
-
-            <VSpacerComponent space={6} />
-           
-            {/* image */}
-            <div className="cart_content__image_holder">
-                <img 
-                    src={food.img}
-                    className="cart_content__image"
-                />
-            </div>
-
-            <VSpacerComponent space={2} />
-
-            {/* name */}
-            <IonTitle className="cart_content__name">
-                { food.name }
-                <span className="cart_content__name_2">
-                    Pizza
-                </span>
-            </IonTitle>
-
-            <VSpacerComponent space={1} />
-
-            {/* type */}
-            <div className="cart_content__type">
-                <IonChip
-                    className="app_chip"
-                    style={{
-                        color: 'white',
-                        fontWeight: '700',
-                        backgroundColor: '#F89A1C',
-                        textTransform: 'capitalize',
-                    }}
-                >
-                    {food.type}
-                </IonChip>     
-            </div>
-
-            <VSpacerComponent space={3} />
-
-            {/* size */}
-            <h5 style={{
-                fontSize: '.9rem',
-                fontWeight: 'bold',
-            }}>
-                SIZE
-            </h5>
-            <div className="cart_content__sizes">
-                {
-                    sizes.map(size=> {
-
-                        return (
-                            <IonChip
-                                className=""
-                                style={{
-                                    color: size === selectedSize ? 'white' : '#F89A1C',
-                                    backgroundColor: size === selectedSize ? '#F89A1C' : 'white',
-                                    border: size === selectedSize ? 'none' : '1px solid #F89A1C',
-                                    textTransform: 'capitalize',
-                                }}
-                                onClick={
-                                    ()=> setSelectedSize(size)
-                                }
-                            >
-                                {size}
-                            </IonChip>
-                        )
-                    })
-                }            
-            </div>
-
-            <VSpacerComponent space={3} />
-
-            {/* quantity */}
-            <h5 style={{
-                fontSize: '.9rem',
-                fontWeight: 'bold',
-            }}>
-                SIZE
-            </h5>
-            <div className="cart_content__quantity">
-
-                <div className="cart_content__quantity__buttons">
-                    <QtyButton 
-                        onClick={addQuantity}
-                        type="add"
-                    />
-                    {quantity}
-                    <QtyButton 
-                        onClick={minusQuantity}
-                        type="remove"
-                    />
-                </div>
-
-                <p>
-                    { food.price*quantity }
-                </p>
-            </div>
-
-
-            {/* toppings */}
-
-
-            {/* place order */}
-
-           
-        </div>
-    )
-}
-
-
-interface QtyButtonProps {
-    onClick: React.MouseEventHandler<HTMLDivElement>,
-    type: string,
-}
-const QtyButton: React.FC<QtyButtonProps> = ({ onClick, type })=> {
-    return (
-        <div onClick={onClick} 
-            className="quantity_button"
-            style={{
-                marginRight: type === 'add' ? '8px' : '0',
-                marginLeft: type === 'add' ? '0' : '8px',
-            }}>
-            <IonIcon 
-                icon={
-                    type === 'add' ? add : remove
-                } 
-            />
-        </div>
-    )
-}
-
 
 export default CartComponent
 
